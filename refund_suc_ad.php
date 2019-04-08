f4905a6019356e44b1ba748f651f225d6ef15e30
tunacup <49384538+tunacup@users.noreply.github.com>
2019-04-08 14:29:24 +0700

    Update refund_suc_ad.php

--- refund_suc_ad.php
+++ refund_suc_ad.php
@@ -22,7 +22,7 @@ $result1 = $mysqli->query($q1);
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
-  <link rel="stylesheet" href="theme.css" type="text/css">
+  <link rel="stylesheet" href="/stylesheets/theme.css" type="text/css">
 </head>
 
 <body>
