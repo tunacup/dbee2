eb1cca4c15bf316e2ae1a1fbbb76e7eb0efb170a
tunacup <49384538+tunacup@users.noreply.github.com>
2019-04-08 14:31:02 +0700

    Update blankC.php

--- blankC.php
+++ blankC.php
@@ -32,7 +32,7 @@ $_SESSION["price"]='2500';
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <link rel="stylesheet" href="theme.css" type="text/css">
+  <link rel="stylesheet" href="/stylesheets/theme.css" type="text/css">
 </head>
 
 <body >
